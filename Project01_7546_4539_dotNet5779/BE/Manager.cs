d69e36698382e2a7dfbb3d73e222124d6b5ccc3d
yishai badich <yibadich@g.jct.ac.il>
2019-01-07 14:47:58 +0200

    שינויים לפני דהמרצה בא לעזור

---
+++ Project01_7546_4539_dotNet5779/BE/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    class Manager
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string Password { get; set; }
+
+        public Manager(string id, string firstName, string password)
+        {
+            ID = id;
+            FirstName = firstName;
+            Password = password;
+        }
+    }
+}
